259a4f1e0c30c0f443a7bc9451869c4fd4110af2
Guilherme Pagio <116508581+gPagio@users.noreply.github.com>
2024-05-20 18:07:30 -0300

    feat: mapeamento fields
    Mapeia as propriedades da API nos campos dos respectivos records
    
    Adiciona o campo id ao livro, o mesmo havia faltado anteriormente

--- src/main/java/br/com/alura/literalura/model/Autor.java
+++ src/main/java/br/com/alura/literalura/model/Autor.java
@@ -1,6 +1,10 @@
 package br.com.alura.literalura.model;
 
-public record Autor (String nome,
-                     Integer anoNascimento,
-                     Integer anoFalecimento) {
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record Autor (@JsonAlias("name") String nome,
+                     @JsonAlias("birth_year") Integer anoNascimento,
+                     @JsonAlias("death_year") Integer anoFalecimento) {
 }
--- src/main/java/br/com/alura/literalura/model/Livro.java
+++ src/main/java/br/com/alura/literalura/model/Livro.java
@@ -1,9 +1,14 @@
 package br.com.alura.literalura.model;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.util.List;
 
-public record Livro(String titulo,
-                    List<Autor> autores,
-                    String idiomas,
-                    Integer numeroDeDownloads) {
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record Livro(@JsonAlias("id") Integer id,
+                    @JsonAlias("title") String titulo,
+                    @JsonAlias("authors") List<Autor> autores,
+                    @JsonAlias("languages") List<String> idiomas,
+                    @JsonAlias("download_count") Integer numeroDeDownloads) {
 }
