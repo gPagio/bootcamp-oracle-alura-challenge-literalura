48c28d5628537fdf661d4ff23f39550db0023fbd
Guilherme Pagio <116508581+gPagio@users.noreply.github.com>
2024-05-20 18:10:09 -0300

    feat: armazena os livros na lista e exibe os mesmos
    Tem a intenção de validar a serializacao do json

--- src/main/java/br/com/alura/literalura/principal/Principal.java
+++ src/main/java/br/com/alura/literalura/principal/Principal.java
@@ -1,17 +1,25 @@
 package br.com.alura.literalura.principal;
 
+import br.com.alura.literalura.model.Livro;
 import br.com.alura.literalura.service.ConsumoApi;
 import br.com.alura.literalura.service.ConverteDados;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Principal {
 
     private static final String ENDERECO_API = "https://gutendex.com/books/";
     private ConverteDados converteDados = new ConverteDados();
+    private List<Livro> livros = new ArrayList<>();
 
     public void exibeMenu(){
-        String responseBody = ConsumoApi.obterDados(ENDERECO_API);
+        String responseBody = converteDados.obterPropriedadeJsonEspecifica(ConsumoApi.obterDados(ENDERECO_API), "results");
 
-        System.out.println("Response Body " + converteDados.obterPropriedadeJsonEspecifica(responseBody, "results"));
+        System.out.println("Response Body " + responseBody);
+        livros = converteDados.serializaLista(responseBody, Livro.class);
+        livros.stream().forEach(System.out::println);
+
         System.out.println("Opa");
     }
 
