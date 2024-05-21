4d849acdf4eec64907377b03869b8372854069de
Guilherme Pagio <116508581+gPagio@users.noreply.github.com>
2024-05-21 09:14:36 -0300

    refactor: ajusta nomenclatura dos records
    Os records renomeados em [31833dfac2e7d3be3a5faef3c7b7390595096c45] surtiram efeitos nesse class, sendo necessário renomealos por aqui também

--- src/main/java/br/com/alura/literalura/principal/Principal.java
+++ src/main/java/br/com/alura/literalura/principal/Principal.java
@@ -1,6 +1,6 @@
 package br.com.alura.literalura.principal;
 
-import br.com.alura.literalura.model.Livro;
+import br.com.alura.literalura.model.DadosLivro;
 import br.com.alura.literalura.service.ConsumoApi;
 import br.com.alura.literalura.service.ConverteDados;
 
@@ -11,13 +11,13 @@ public class Principal {
 
     private static final String ENDERECO_API = "https://gutendex.com/books/";
     private ConverteDados converteDados = new ConverteDados();
-    private List<Livro> livros = new ArrayList<>();
+    private List<DadosLivro> livros = new ArrayList<>();
 
     public void exibeMenu(){
         String responseBody = converteDados.obterPropriedadeJsonEspecifica(ConsumoApi.obterDados(ENDERECO_API), "results");
 
         System.out.println("Response Body " + responseBody);
-        livros = converteDados.serializaLista(responseBody, Livro.class);
+        livros = converteDados.serializaLista(responseBody, DadosLivro.class);
         livros.stream().forEach(System.out::println);
 
         System.out.println("Opa");
