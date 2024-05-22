5707b18ad72df6c1dca71695213c5858a97591e1
Guilherme Pagio <116508581+gPagio@users.noreply.github.com>
2024-05-22 17:46:40 -0300

    feat: inicia um servico junto com aplicacao
    Inicia o servico de busca de livros junto a aplicacao, permitindo que o Spring Boot gerencia as dependencias do serviço

--- src/main/java/br/com/alura/literalura/LiteraluraApplication.java
+++ src/main/java/br/com/alura/literalura/LiteraluraApplication.java
@@ -1,6 +1,8 @@
 package br.com.alura.literalura;
 
 import br.com.alura.literalura.principal.Principal;
+import br.com.alura.literalura.service.LivroService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,13 +10,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LiteraluraApplication implements CommandLineRunner {
 
+	@Autowired
+	private LivroService livroService;
+
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraluraApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		Principal p = new Principal();
+
+		Principal p = new Principal(livroService);
 		p.exibeMenu();
 	}
 }
