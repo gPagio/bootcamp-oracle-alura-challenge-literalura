3ac5fca46244e80b0b8023be5f443acddb123cab
gPagio <guilhermepagio.contato@gmail.com>
2024-05-22 21:47:28 -0300

    feat: metodos busca autores
    Metodos de busca de autores

--- src/main/java/br/com/alura/literalura/repository/LivroRepository.java
+++ src/main/java/br/com/alura/literalura/repository/LivroRepository.java
@@ -1,11 +1,20 @@
 package br.com.alura.literalura.repository;
 
+import br.com.alura.literalura.model.Autor;
 import br.com.alura.literalura.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface LivroRepository extends JpaRepository<Livro, Long> {
 
     Optional<Livro> findByTituloContainingIgnoreCase(String tituloLivro);
+
+    @Query("SELECT a FROM Livro l JOIN l.autores a")
+    List<Autor> buscarTodosOsAutores();
+
+    @Query("SELECT a FROM Livro l JOIN l.autores a WHERE :ano < a.anoFalecimento")
+    List<Autor> buscarAutoresVivosEmDeterminadoAno(int ano);
 }
