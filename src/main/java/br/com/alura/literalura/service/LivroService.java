d68f455ecf2a7d51112ff8ece7213c165aa8ecf8
gPagio <guilhermepagio.contato@gmail.com>
2024-05-22 21:48:03 -0300

    feat: metodos busca autores
    Metodos de busca de autores

--- src/main/java/br/com/alura/literalura/service/LivroService.java
+++ src/main/java/br/com/alura/literalura/service/LivroService.java
@@ -1,6 +1,8 @@
 package br.com.alura.literalura.service;
 
+import br.com.alura.literalura.dto.AutorDTO;
 import br.com.alura.literalura.dto.LivroDTO;
+import br.com.alura.literalura.model.Autor;
 import br.com.alura.literalura.model.Livro;
 import br.com.alura.literalura.repository.LivroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,11 +30,23 @@ public class LivroService {
         return null;
     }
 
+    public List<AutorDTO> buscarTodosOsAutores(){
+        return listAutorToListAutorDTO(repository.buscarTodosOsAutores());
+    }
+
     private LivroDTO livroToLivroDTO(Livro l){
         return new LivroDTO(l);
     }
 
+    public List<AutorDTO> buscarAutoresVivosEmDeterminadoAno(int ano) {
+        return listAutorToListAutorDTO(repository.buscarAutoresVivosEmDeterminadoAno(ano));
+    }
+
     private List<LivroDTO> listLivroToListLivroDTO(List<Livro> listLivro) {
         return listLivro.stream().map(livro -> new LivroDTO(livro)).collect(Collectors.toList());
+    }
+
+    private List<AutorDTO> listAutorToListAutorDTO(List<Autor> listAutor){
+        return listAutor.stream().map(autor -> new AutorDTO(autor)).collect(Collectors.toList());
     }
 }
